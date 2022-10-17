13864d3ea756752b15499509886e0d51f0842f97
sangkips <sangkipkoech@gmail.com>
2022-10-17 20:32:49 +0300

    Implemented basic Authentication

--- restapi/models.py
+++ restapi/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     task = models.TextField()
+    owner = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
--- restapi/serializers.py
+++ restapi/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from .models import Todo
 from django.contrib.auth.models import User, Group
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    todos = serializers.PrimaryKeyRelatedField(many=True, queryset=Todo.objects.all())
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'email', 'is_staff', 'todos']
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
