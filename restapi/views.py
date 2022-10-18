252ca475e614e953eec441ebd1499ec212b3d1e4
sangkips <sangkipkoech@gmail.com>
2022-10-18 23:01:56 +0300

    Set the User to operated CRUD functions to be admin only

--- restapi/views.py
+++ restapi/views.py
@@ -4,15 +4,16 @@ from rest_framework import permissions
 from .serializers import TodoSerializer, UserSerializer, GroupSerializer
 from django.contrib.auth.models import User, Group
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
