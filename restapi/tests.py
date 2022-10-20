87e17f5608fa2da37e1948b2daaab99d2bc5c2ab
sangkips <sangkipkoech@gmail.com>
2022-10-20 17:14:37 +0300

    added unittest file

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from . models import Todo
 
 # Create your tests here.
+
+class TestCaseTodo(TestCase):
+    def testTodo(self):
+        todo = Todo(title="create django app", task="This is a django application")
+        self.assertEqual(todo.title, "create django app")
+        self.assertEqual(todo.task, "This is a django application")
